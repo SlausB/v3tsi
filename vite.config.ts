997c963d5831f1fa3d112e7f8db33b3a4fc62f34
Slaus <SlavMFM@gmail.com>
2024-10-29 23:27:31 +0400

    finally successfully published on Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/v3tsi/',
   plugins: [vue()],
   resolve: {
     alias: {
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/v3tsi/' : '/',
+    publicPath: '/v3tsi/',
 };
